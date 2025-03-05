971ba6277c9861d95bdccac86d9192163c796407
Cho Seonghan <chosnhn1@gmail.com>
2025-03-05 17:47:54 +0900

    write npm script for build

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -9,11 +10,19 @@ dotenv.config();
 // init; default PORT fallbacks to 5000
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // setting for use json
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // index
 app.get("/", (request, response) => {
